e090a93c054137d826366fdc6d80d7770a41950d
gpovidaiko <gpovidaiko@hotmail.com>
2023-09-22 12:32:20 -0300

    Formatação de código
    Ajustes gerais de formatação de código.

--- assets/js/keyboard-navigation.js
+++ assets/js/keyboard-navigation.js
@@ -1,8 +1,8 @@
-const homeAnchor = document.querySelector("a.home__link");
-const menuAnchors = document.querySelectorAll("a.nav__link");
+const homeAnchor = document.querySelector('a.home__link');
+const menuAnchors = document.querySelectorAll('a.nav__link');
 
-window.addEventListener("keydown", (event) => {
-	if (event.key === "0") {
+window.addEventListener('keydown', event => {
+	if (event.key === '0') {
 		homeAnchor.click();
 	}
 	if (Number(event.key) >= 1 && Number(event.key) <= 5) {
